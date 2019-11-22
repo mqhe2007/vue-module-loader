41daae4a349be4e4d9fc197b39f4b1c9b80c424b
青衿 <122274389@qq.com>
2019-11-22 14:26:59 +0800

    加载模块函数时增加传入Vue构造函数

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import eventBus from './ability/eventBus'
 import moduleLoader from './ability/moduleLoader'
 export default function(Vue, options = {}) {
   Vue.prototype.$eventBus = eventBus()
-  Vue.prototype.$moduleLoader = moduleLoader
+  Vue.prototype.$moduleLoader = moduleLoader(Vue)
   if (options.store) {
     options.store.registerModule('dynamicComponent', dynamicComponentState)
     Vue.prototype.$dynamicComponent = dynamicComponent(options.store)
